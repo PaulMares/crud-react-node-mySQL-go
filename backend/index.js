e9b2433eaa15845c3370507d0a14e65b2a9ca0dc
PaulMares <fpmaresquiersanchez@dons.usfca.edu>
2025-03-03 06:27:30 -0800

    FIX(backend): idk

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,6 @@ const db = mysql.createConnection({
 
 app.use(cors())
 
-app.use(express.json())//return json data using the api server postman
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
