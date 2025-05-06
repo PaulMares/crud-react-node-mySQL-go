9807a51a656737167f4ce5d3ebf352b21e167c6f
PaulMares <fpmaresquiersanchez@dons.usfca.edu>
2025-05-05 17:28:24 -0700

    Add CNAME

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,24 +9,25 @@ export default defineConfig({
 		port: 80,
 		cors: true,
 		proxy: {
-			'/api/backend': {
-				target: 'http://backend.qa.svc.cluster.local:8800',
+			"/api/backend": {
+				target: "http://backend.qa.svc.cluster.local:8800",
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/backend/, '')
+				rewrite: (path) => path.replace(/^\/api\/backend/, "")
 			},
-			'/api/api-1': {
-				target: 'http://api-1.qa.svc.cluster.local:8802',
+			"/api/api-1": {
+				target: "http://api-1.qa.svc.cluster.local:8802",
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/api-1/, '')
+				rewrite: (path) => path.replace(/^\/api\/api-1/, "")
 			},
-			'/api/api-2': {
-				target: 'http://api-2.qa.svc.cluster.local:8804',
+			"/api/api-2": {
+				target: "http://api-2.qa.svc.cluster.local:8804",
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/api-2/, '')
+				rewrite: (path) => path.replace(/^\/api\/api-2/, "")
 			}
 		},
 		allowedHosts: [
-			"ad2c8a8a162124b85a35e2f2a019e9a0-1081224088.us-east-1.elb.amazonaws.com"
+			"ad2c8a8a162124b85a35e2f2a019e9a0-1081224088.us-east-1.elb.amazonaws.com",
+			"pauldoestech.dev"
 		]
 	}
 });
